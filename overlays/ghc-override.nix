393dcdd7f0673015feb510f65ed3de965625128e
armin-th <armin@eigenfunctor.io>
2021-06-07 00:25:15 -0400

    [FIX] use override and overrideAttrs instead of modified nixpkgs source file

--- overlays/ghc-override.nix
+++ overlays/ghc-override.nix
@@ -1,17 +1,29 @@
 self: super:
   let
     ghcVersion = "ghc8104";
+    targetCC = super.pkgsBuildTarget.targetPackages.stdenv.cc;
+    notGoldFlag = flag: (builtins.match ".*gold.*" flag) == null;
   in {
     haskell = (super.haskell // {
       packages = (super.haskell.packages // {
         ${ghcVersion} = (super.haskell.packages.${ghcVersion}.override {
-          ghc = (super.buildPackages.callPackage ../pkgs/ghc8104.nix {
-            disableLdGold = true;
-            bootPkgs = super.buildPackages.haskell.packages.ghc865Binary;
-            inherit (super.python3Packages) sphinx;
-            buildLlvmPackages = super.buildPackages.llvmPackages_11;
-            llvmPackages = super.llvmPackages_11;
-          });
+          ghc =
+            let
+              ghcLLVM11 = super.buildPackages.haskell.compiler.${ghcVersion}
+                .override({
+                  buildLlvmPackages = super.buildPackages.llvmPackages_11;
+                  llvmPackages = super.llvmPackages_11;
+                });
+              ghcWithAtomicFix = ghcLLVM11.overrideAttrs(old: {
+                patches = [../patches/riscv64-ghc-atomic-fix.patch] ++ (old.patches or []);
+                preConfigure = ''
+                  ${old.preConfigure}
+                  export LD="${targetCC.bintools}/bin/${targetCC.bintools.targetPrefix}ld"
+                  NIX_LDFLAGS+=" -latomic"
+                '';
+                configureFlags = builtins.filter notGoldFlag (old.configureFlags or []);
+              });
+            in ghcWithAtomicFix;
         });
       });
     });
